4239b3d8d50cc78e1f45a8f7c8f0788a33339f95
github-action-benchmark <github@users.noreply.github.com>
2025-08-25 20:01:59 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 992ea776e856edd74f9313f30cee7ed23dbc470f

---
+++ dev/bench/data.js
@@ -0,0 +1,52 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1756152119221,
+  "repoUrl": "https://github.com/tdrwenski/RAJAPerf",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "drwenski1@llnl.gov",
+            "name": "Tara Drwenski",
+            "username": "tdrwenski"
+          },
+          "committer": {
+            "email": "drwenski1@llnl.gov",
+            "name": "Tara Drwenski",
+            "username": "tdrwenski"
+          },
+          "distinct": true,
+          "id": "992ea776e856edd74f9313f30cee7ed23dbc470f",
+          "message": "WIP",
+          "timestamp": "2025-08-25T13:58:10-06:00",
+          "tree_id": "e4fa8451545bde35556ec2d19abd774bd4d2a40a",
+          "url": "https://github.com/tdrwenski/RAJAPerf/commit/992ea776e856edd74f9313f30cee7ed23dbc470f"
+        },
+        "date": 1756152118803,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Basic_DAXPY_Base_Seq",
+            "value": 108.151,
+            "unit": "ms"
+          },
+          {
+            "name": "Basic_DAXPY_RAJA_Seq",
+            "value": 174.119,
+            "unit": "ms"
+          },
+          {
+            "name": "Basic_PI_ATOMIC_Base_Seq",
+            "value": 71.843,
+            "unit": "ms"
+          },
+          {
+            "name": "Basic_PI_ATOMIC_RAJA_Seq",
+            "value": 70.22,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
