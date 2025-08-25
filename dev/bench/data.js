5f264f48a47e083dc643e9a2680e14db79f2b5fb
github-action-benchmark <github@users.noreply.github.com>
2025-08-25 20:09:40 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 361004436a3f5cef3a12cbc63041d9b05c9dd841

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1756152119221,
+  "lastUpdate": 1756152580199,
   "repoUrl": "https://github.com/tdrwenski/RAJAPerf",
   "entries": {
     "Benchmark": [
@@ -43,6 +43,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "Basic_PI_ATOMIC_RAJA_Seq",
             "value": 70.22,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "drwenski1@llnl.gov",
+            "name": "Tara Drwenski",
+            "username": "tdrwenski"
+          },
+          "committer": {
+            "email": "drwenski1@llnl.gov",
+            "name": "Tara Drwenski",
+            "username": "tdrwenski"
+          },
+          "distinct": true,
+          "id": "361004436a3f5cef3a12cbc63041d9b05c9dd841",
+          "message": "WIP Add benchmark workflows",
+          "timestamp": "2025-08-25T14:05:30-06:00",
+          "tree_id": "4821ee7a6d6f7706ebfa5ed5e24c62867f75d2d0",
+          "url": "https://github.com/tdrwenski/RAJAPerf/commit/361004436a3f5cef3a12cbc63041d9b05c9dd841"
+        },
+        "date": 1756152579143,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Basic_DAXPY_Base_Seq",
+            "value": 107.00800000000001,
+            "unit": "ms"
+          },
+          {
+            "name": "Basic_DAXPY_RAJA_Seq",
+            "value": 158.00300000000001,
+            "unit": "ms"
+          },
+          {
+            "name": "Basic_PI_ATOMIC_Base_Seq",
+            "value": 70.48,
+            "unit": "ms"
+          },
+          {
+            "name": "Basic_PI_ATOMIC_RAJA_Seq",
+            "value": 70.12400000000001,
             "unit": "ms"
           }
         ]
