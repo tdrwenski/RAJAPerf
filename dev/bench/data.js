68027557cea1d5eb6d7506d7753957cd91c2e5a6
github-action-benchmark <github@users.noreply.github.com>
2025-08-25 22:40:07 +0000

    add RAJAPerf with Caliper (customSmallerIsBetter) benchmark result for 403478a625cb9ad463612a4802102cd19e28cf28

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1756161181726,
+  "lastUpdate": 1756161607084,
   "repoUrl": "https://github.com/tdrwenski/RAJAPerf",
   "entries": {
     "RAJAPerf": [
@@ -39,6 +39,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "Basic_PI_ATOMIC_RAJA_Seq",
             "value": 70.077,
+            "unit": "ms"
+          }
+        ]
+      }
+    ],
+    "RAJAPerf with Caliper": [
+      {
+        "commit": {
+          "author": {
+            "name": "tdrwenski",
+            "username": "tdrwenski"
+          },
+          "committer": {
+            "name": "tdrwenski",
+            "username": "tdrwenski"
+          },
+          "id": "403478a625cb9ad463612a4802102cd19e28cf28",
+          "message": "Test GitHub benchmark",
+          "timestamp": "2025-08-22T17:24:09Z",
+          "url": "https://github.com/tdrwenski/RAJAPerf/pull/1/commits/403478a625cb9ad463612a4802102cd19e28cf28"
+        },
+        "date": 1756161606335,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Basic_DAXPY_Base_Seq",
+            "value": 160.32399999999998,
+            "unit": "ms"
+          },
+          {
+            "name": "Basic_PI_ATOMIC_Base_Seq",
+            "value": 70.119,
+            "unit": "ms"
+          },
+          {
+            "name": "Basic_DAXPY_RAJA_Seq",
+            "value": 108.171,
+            "unit": "ms"
+          },
+          {
+            "name": "Basic_PI_ATOMIC_RAJA_Seq",
+            "value": 70.298,
             "unit": "ms"
           }
         ]
