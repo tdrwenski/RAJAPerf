d99a13a16a7acc883ba69eb5742c4b3d7541c2ee
github-action-benchmark <github@users.noreply.github.com>
2025-09-11 20:51:07 +0000

    add clang_14_0_6_caliper (customSmallerIsBetter) benchmark result for c412171d4716b64ecefc146066202b258d41c9f7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757620562655,
+  "lastUpdate": 1757623867840,
   "repoUrl": "https://github.com/tdrwenski/RAJAPerf",
   "entries": {
     "clang_14_0_6_caliper": [
@@ -83,6 +83,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "RAJA_Seq_Basic_PI_ATOMIC",
             "value": 0.0699991,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Tara Drwenski",
+            "username": "tdrwenski",
+            "email": "drwenski1@llnl.gov"
+          },
+          "committer": {
+            "name": "Tara Drwenski",
+            "username": "tdrwenski",
+            "email": "drwenski1@llnl.gov"
+          },
+          "id": "c412171d4716b64ecefc146066202b258d41c9f7",
+          "message": "WIP comment out other jobs for testing",
+          "timestamp": "2025-09-11T20:28:17Z",
+          "url": "https://github.com/tdrwenski/RAJAPerf/commit/c412171d4716b64ecefc146066202b258d41c9f7"
+        },
+        "date": 1757623866683,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "RAJA_OpenMP_Basic_DAXPY",
+            "value": 0.00538662,
+            "unit": "s"
+          },
+          {
+            "name": "RAJA_OpenMP_Basic_PI_ATOMIC",
+            "value": 31.22,
+            "unit": "s"
+          },
+          {
+            "name": "RAJA_Seq_Basic_DAXPY",
+            "value": 0.263321,
+            "unit": "s"
+          },
+          {
+            "name": "RAJA_Seq_Basic_PI_ATOMIC",
+            "value": 0.0702807,
             "unit": "s"
           }
         ]
