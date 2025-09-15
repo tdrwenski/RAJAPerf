507d49d1ef4002bc1c8e833bb891b7284648df86
github-action-benchmark <github@users.noreply.github.com>
2025-09-15 20:01:26 +0000

    add rocmcc_6_4_1_hip_openmp_caliper (customSmallerIsBetter) benchmark result for 9faff7b6a3497b074dfcb4a3dfff66ba092660fe

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757966485198,
+  "lastUpdate": 1757966486680,
   "repoUrl": "https://github.com/tdrwenski/RAJAPerf",
   "entries": {
     "clang_14_0_6_caliper": [
@@ -825,6 +825,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "RAJA_HIP_Basic_PI_ATOMIC",
             "value": 0.0076295,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Tara Drwenski",
+            "username": "tdrwenski",
+            "email": "drwenski1@llnl.gov"
+          },
+          "committer": {
+            "name": "Tara Drwenski",
+            "username": "tdrwenski",
+            "email": "drwenski1@llnl.gov"
+          },
+          "id": "9faff7b6a3497b074dfcb4a3dfff66ba092660fe",
+          "message": "WIP turn off other tests",
+          "timestamp": "2025-09-11T22:31:16Z",
+          "url": "https://github.com/tdrwenski/RAJAPerf/commit/9faff7b6a3497b074dfcb4a3dfff66ba092660fe"
+        },
+        "date": 1757966486268,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "RAJA_HIP_Basic_DAXPY",
+            "value": 0.0046357,
+            "unit": "s"
+          },
+          {
+            "name": "RAJA_HIP_Basic_PI_ATOMIC",
+            "value": 0.00758028,
             "unit": "s"
           }
         ]
