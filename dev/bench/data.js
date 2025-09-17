e53e8121faa3d7db1db9f28bebf40059c172362c
github-action-benchmark <github@users.noreply.github.com>
2025-09-17 00:34:46 +0000

    add clang_14_0_6_caliper (customSmallerIsBetter) benchmark result for dfba0619b6fa5184a2d747c5fad78190e37c02af

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758069228633,
+  "lastUpdate": 1758069286325,
   "repoUrl": "https://github.com/tdrwenski/RAJAPerf",
   "entries": {
     "clang_14_0_6_caliper": [
@@ -209,6 +209,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "RAJA_Seq_Basic_PI_ATOMIC",
             "value": 0.0534226,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Tara Drwenski",
+            "username": "tdrwenski",
+            "email": "drwenski1@llnl.gov"
+          },
+          "committer": {
+            "name": "Tara Drwenski",
+            "username": "tdrwenski",
+            "email": "drwenski1@llnl.gov"
+          },
+          "id": "dfba0619b6fa5184a2d747c5fad78190e37c02af",
+          "message": "WIP make test slower on purpose",
+          "timestamp": "2025-09-17T00:26:46Z",
+          "url": "https://github.com/tdrwenski/RAJAPerf/commit/dfba0619b6fa5184a2d747c5fad78190e37c02af"
+        },
+        "date": 1758069285867,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "RAJA_OpenMP_Basic_DAXPY",
+            "value": 0.00898077,
+            "unit": "s"
+          },
+          {
+            "name": "RAJA_OpenMP_Basic_PI_ATOMIC",
+            "value": 2.9023,
+            "unit": "s"
+          },
+          {
+            "name": "RAJA_Seq_Basic_DAXPY",
+            "value": 0.232487,
+            "unit": "s"
+          },
+          {
+            "name": "RAJA_Seq_Basic_PI_ATOMIC",
+            "value": 0.153459,
             "unit": "s"
           }
         ]
